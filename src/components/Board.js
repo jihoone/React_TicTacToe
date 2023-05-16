535f66e6d1de3b1a7c56bae23f7103d36533baad
정지훈 <jungjihoons@naver.com>
2023-05-17 00:48:18 +0900

    #9 - 승자 결정하기
    승리의 경우의 수를 확인하여 일치하면 승자 표시
    X와 O중 누군가 승리하거나 Square가 이미 채워있다면 Board의 handleClick함수가 클릭을 무시하도록 구성

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,12 +6,45 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
-  const status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  }
+  else {
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
-    const newsquares = squares.slice();
-    newsquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(newsquares);
+    const newSquares = squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
     setXIsNext(prev => !prev)
   }
 
